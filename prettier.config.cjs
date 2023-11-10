4cc43e7f505fa2448d653a8102fda89d4223bf41
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-11-10 12:51:42 +0100

    Add prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+const tailwindPlugin = require('prettier-plugin-tailwindcss')
+
+module.exports = {
+    plugins: [tailwindPlugin],
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    singleAttributePerLine: false,
+}
