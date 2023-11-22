bd86b35e4f61409965fdf089680c02cfbbc5995a
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-11-22 17:52:55 +0100

    Create grid on Contact page

--- src/pages/Regulations.jsx
+++ src/pages/Regulations.jsx
@@ -1,9 +1,21 @@
-const list = [{ name: 'aquapark' }, { name: 'sauna' }, { name: 'spa' }, { name: 'parking' }];
+import FaqTab from '../components/FaqTab';
+
+const list = [
+  { name: 'aquapark', regulations: 'regulations' },
+  { name: 'sauna', regulations: 'regulations' },
+  { name: 'spa', regulations: 'regulations' },
+  { name: 'parking', regulations: 'regulations' },
+];
 
 const Regulations = () => {
   return (
     <div>
       <h1>Regulations</h1>
+      <div className="flex flex-col items-start">
+        {list.map((item) => (
+          <FaqTab key={item.name} question={item.name} answer={item.regulations} />
+        ))}
+      </div>
     </div>
   );
 };
