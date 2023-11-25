112b236d9271c1f0316e075f799da66eadb835b3
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-11-25 01:16:45 +0100

    Add flowbite-react

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ArrowRightCircleIcon, ArrowLeftCircleIcon } from '@heroicons/react/20/solid';
+import { Button } from 'flowbite-react';
 
 const images = [
   { id: 1, src: 'https://loremflickr.com/1280/600' },
@@ -25,26 +26,8 @@ const Carousel = () => {
 
   return (
     <>
-      <span>{currentImage.id}</span>
-      <div className="relative h-[600px] w-full self-center bg-red-200">
-        {images.map((image) => (
-          <img
-            key={image.id}
-            src={image.src}
-            alt="Carousel"
-            className={`absolute left-0 top-0 h-full w-full object-cover transition-all duration-300 ${
-              image === currentImage ? 'opacity-100' : 'opacity-0'
-            }`}
-          />
-        ))}
-      </div>
       <div>
-        <button onClick={handleLeftArrowClick}>
-          <ArrowLeftCircleIcon className="h-6 w-6 text-gray-500" />
-        </button>
-        <button onClick={handleRightArrowClick}>
-          <ArrowRightCircleIcon className="h-6 w-6 text-gray-500" />
-        </button>
+        <Button>Click me</Button>
       </div>
     </>
   );
